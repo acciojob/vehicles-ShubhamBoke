9cab1c4271cd49e8c5e3f7c7696badd7df22d020
Shubham Boke <shubhamboke.99@gmail.com>
2023-06-10 15:18:15 +0530

    Correction 3

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -20,6 +20,17 @@ public class Car extends Vehicle {
         this.seats = seats;
     }
 
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type) {
+        super(name);
+        //Hint: Car extends Vehicle
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = 4;
+    }
+
     public void changeGear(int newGear){
         this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
